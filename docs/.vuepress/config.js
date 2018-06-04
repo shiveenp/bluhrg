9f5c0f1649c280da73dc0955d8264094a0ffe426
samdbeckham <sbeckham@gitlab.com>
2018-06-04 16:25:03 +0100

    Initial commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    title: 'GitLab ❤️ VuePress',
+    description: 'Vue-powered static site generator running on Gitlab Pages',
+    base: 'vuepress-gl-pages',
+    dest: 'public'
+}
