c7c9aae60652b34e32dea0e3a70535eaef2a04b2
Achilleas Pipinellis <axil@gitlab.com>
2018-08-06 10:16:45 +0000

    Change base URL

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     title: 'GitLab ❤️ VuePress',
     description: 'Vue-powered static site generator running on Gitlab Pages',
-    base: '/vuepress-gl-pages/',
+    base: '/vuepress/',
     dest: 'public'
 }
