9fb6298d79eb9c296e3f01567e5c7eff7f7594f2
Achilleas Pipinellis <axil@gitlab.com>
2018-08-06 10:19:42 +0000

    Correct GitLab name

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     title: 'GitLab ❤️ VuePress',
-    description: 'Vue-powered static site generator running on Gitlab Pages',
+    description: 'Vue-powered static site generator running on GitLab Pages',
     base: '/vuepress/',
     dest: 'public'
 }
