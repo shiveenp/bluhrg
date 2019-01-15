fc036f197b37a91d9d034b7672e8cbe5f936e1b6
shiveenpandita <shiveenpandita@gmail.com>
2019-01-15 20:38:37 +1100

    changes to configs for fixing deploy issues with netlify

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,6 @@
 module.exports = {
   title: 'Shiveens Blog',
   description: 'Thoughts and chronicles on programming 👨‍💻 and nature 🌲',
-  base: '/blog/',
-  dest: 'public',
   head: [
     ['link', {rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css'}]
   ],
